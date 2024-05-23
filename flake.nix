b05c4c706f323bd76b3b2827d82bad6b5aafa8cc
Jake Hamilton <jake.hamilton@hey.com>
2024-05-22 23:01:23 -0700

    chore: pin inputs

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
     flake-compat = {
-      url = "github:edolstra/flake-compat";
+      url = "github:edolstra/flake-compat?ref=v1.0.1";
       flake = false;
     };
 
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
+      url = "github:snowfallorg/lib?ref=v3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
